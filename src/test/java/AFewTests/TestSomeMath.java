0f6ae7fb0d9fdd1252bb03e211bd7218f26b1c1f
Charles Bartels <charlieb2898@gmail.com>
2020-11-09 10:28:24 -0600

    added a test case

---
+++ src/test/java/AFewTests/TestSomeMath.java
@@ -0,0 +1,23 @@
+package AFewTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestSomeMath {
+
+    private int x;
+    private int y;
+
+    @BeforeEach
+    public void setUp(){
+        x = 10;
+        y = 3;
+    }
+
+    @Test
+    public void testAddition(){
+        assertEquals(x + y, 13);
+    }
+}
